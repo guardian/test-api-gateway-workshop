e03f9bf0fb7d088c2fa7f39142e3d5ee36f706d4
vlbee <32312712+vlbee@users.noreply.github.com>
2019-02-07 15:20:05 +0000

    Add json to Api Response

---
+++ src/main/scala/com/gu/ApiResponse.scala
@@ -0,0 +1,14 @@
+package com.gu.testApiGatewayWorkshop
+
+import io.circe.Encoder
+import io.circe.syntax._
+import io.circe.generic.extras.semiauto._
+
+
+case class APIResponse(statusCode: Int, headers: Map[String, String], body: String)
+
+object APIResponse {
+
+  implicit val APIResponseEncoder : Encoder[APIResponse] = deriveEncoder
+
+}
